bb1c863e664a81883ab0d20da14cdce61091696a
mrvershynin <mrvershynin@gmail.com>
2019-11-03 22:43:38 +0200

    eslint refactoring code

--- dist/app.js
+++ dist/app.js
@@ -14,58 +14,58 @@ var searchTitle = document.getElementById('search');
 
 //Open modal window with form for create new task
 createButton.onclick = function () {
-	modal.style.display = "block";
+  modal.style.display = "block";
 };
 //Close modal window and clear inputs after click on button "Cancel"
 function closeModal() {
-	modal.style.display = "none";
-	document.getElementById('new-title').value = '';
-	document.getElementById('new-description').value = '';
+  modal.style.display = "none";
+  document.getElementById('new-title').value = '';
+  document.getElementById('new-description').value = '';
 }
 cancelButton.onclick = closeModal;
 
 //
 function createNewElement(title, description, priority) {
-	var listItem = document.createElement('li');
-	listItem.className = 'task' + ' ' + 'item' + ' ' + priority;
-	listItem.style.display = 'block';
-	var titleTask = document.createElement('title');
-	titleTask.innerText = title;
-	var descriptionTask = document.createElement('description');
-	descriptionTask.innerText = description;
-	var priorityTask = document.createElement('priority');
-	priorityTask.innerText = priority;
-	var multiButton = document.createElement('button');
-	multiButton.innerText = '...';
-	var listContainer = document.createElement('ul');
-	listContainer.className = 'list-container';
-	var doneButton = document.createElement('li');
-	doneButton.innerText = 'Done';
-	doneButton.className = 'done';
-	var editButton = document.createElement('li');
-	editButton.innerText = 'Edit';
-	editButton.className = 'edit';
-	var deleteButton = document.createElement('li');
-	deleteButton.innerText = 'Delete';
-	deleteButton.className = 'delete';
-	listItem.appendChild(titleTask);
-	listItem.appendChild(descriptionTask);
-	listItem.appendChild(priorityTask);
-	listItem.appendChild(multiButton);
-	listItem.appendChild(listContainer);
-	listContainer.appendChild(doneButton);
-	listContainer.appendChild(editButton);
-	listContainer.appendChild(deleteButton);
-	multiButton.onclick = openList;
-	return listItem;
+  var listItem = document.createElement('li');
+  listItem.className = 'task' + ' ' + 'item' + ' ' + priority;
+  listItem.style.display = 'block';
+  var titleTask = document.createElement('title');
+  titleTask.innerText = title;
+  var descriptionTask = document.createElement('description');
+  descriptionTask.innerText = description;
+  var priorityTask = document.createElement('priority');
+  priorityTask.innerText = priority;
+  var multiButton = document.createElement('button');
+  multiButton.innerText = '...';
+  var listContainer = document.createElement('ul');
+  listContainer.className = 'list-container';
+  var doneButton = document.createElement('li');
+  doneButton.innerText = 'Done';
+  doneButton.className = 'done';
+  var editButton = document.createElement('li');
+  editButton.innerText = 'Edit';
+  editButton.className = 'edit';
+  var deleteButton = document.createElement('li');
+  deleteButton.innerText = 'Delete';
+  deleteButton.className = 'delete';
+  listItem.appendChild(titleTask);
+  listItem.appendChild(descriptionTask);
+  listItem.appendChild(priorityTask);
+  listItem.appendChild(multiButton);
+  listItem.appendChild(listContainer);
+  listContainer.appendChild(doneButton);
+  listContainer.appendChild(editButton);
+  listContainer.appendChild(deleteButton);
+  multiButton.onclick = openList;
+  return listItem;
 }
 
 //
 function addTask() {
-	var listItem = createNewElement(newTitle.value, newDescription.value, priorityTask.value);
-	tasksList.appendChild(listItem);
-	bindTaskEvent(listItem, doneTask);
-	closeModal();
+  var listItem = createNewElement(newTitle.value, newDescription.value, priorityTask.value);
+  tasksList.appendChild(listItem);
+  bindTaskEvent(listItem, doneTask);
+  closeModal();
 }
 
 //
@@ -73,99 +73,99 @@ saveButton.onclick = addTask;
 
 //Click button for open list with edit buttons
 function openList() {
-	var container = this.parentNode;
-	var list = container.querySelector('ul.list-container');
-	list.style.display = list.style.display == 'block' ? 'none' : 'block';
+  var container = this.parentNode;
+  var list = container.querySelector('ul.list-container');
+  list.style.display = list.style.display == 'block' ? 'none' : 'block';
 }
 
 //Button done
 function doneTask() {
-	var listItem = this.parentNode;
-	var taskBody = listItem.parentNode;
-	var doneButton = listItem.querySelector('li.done');
-	var checkBox = document.createElement('input');
-	checkBox.setAttribute('type', 'checkbox');
-	checkBox.checked = true;
-	taskBody.appendChild(checkBox);
-	taskBody.className = 'task-done' + ' ' + 'item' + ' ' + 'priority';
-	checkBox.onclick = unDoneTask;
+  var listItem = this.parentNode;
+  var taskBody = listItem.parentNode;
+  var doneButton = listItem.querySelector('li.done');
+  var checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.checked = true;
+  taskBody.appendChild(checkBox);
+  taskBody.className = 'task-done' + ' ' + 'item' + ' ' + 'priority';
+  checkBox.onclick = unDoneTask;
 }
 //Mark task as not completed
 function unDoneTask() {
-	var li = this.parentNode;
-	li.className = 'task' + ' ' + 'item' + ' ' + 'priority';
-	var checked = li.querySelector('input');
-	li.removeChild(checked);
+  var li = this.parentNode;
+  li.className = 'task' + ' ' + 'item' + ' ' + 'priority';
+  var checked = li.querySelector('input');
+  li.removeChild(checked);
 }
 function editTask() {
-	var list = this.parentNode;
-	var ul = list.parentNode;
-	var task = ul.parentNode;
-	modal.style.display = 'block';
-	var changeTitle = task.querySelector('title').textContent;
-	var changeDescription = task.querySelector('description').textContent;
-	newTitle.value = changeTitle;
-	newDescription.value = changeDescription;
-	task.removeChild(ul);
+  var list = this.parentNode;
+  var ul = list.parentNode;
+  var task = ul.parentNode;
+  modal.style.display = 'block';
+  var changeTitle = task.querySelector('title').textContent;
+  var changeDescription = task.querySelector('description').textContent;
+  newTitle.value = changeTitle;
+  newDescription.value = changeDescription;
+  task.removeChild(ul);
 }
 //Button delete task
 function deleteTask() {
-	var list = this.parentNode;
-	var ul = list.parentNode;
-	var task = ul.parentNode;
-	task.removeChild(ul);
+  var list = this.parentNode;
+  var ul = list.parentNode;
+  var task = ul.parentNode;
+  task.removeChild(ul);
 }
 function bindTaskEvent(listItem, statusEvent) {
-	var doneButton = listItem.querySelector('li.done');
-	var editButton = listItem.querySelector('li.edit');
-	var deleteButton = listItem.querySelector('li.delete');
+  var doneButton = listItem.querySelector('li.done');
+  var editButton = listItem.querySelector('li.edit');
+  var deleteButton = listItem.querySelector('li.delete');
 
-	doneButton.onclick = doneTask;
-	editButton.onclick = editTask;
-	deleteButton.onclick = deleteTask;
+  doneButton.onclick = doneTask;
+  editButton.onclick = editTask;
+  deleteButton.onclick = deleteTask;
 }
 
 //Filter by status
 var filterStatus = document.getElementById('select-status');
 var itemsElementsStatus = document.getElementById('tasks');
 filterStatus.onchange = function () {
-	var itemsSt = itemsElementsStatus.getElementsByClassName('item');
-	for (var i = 0; i < itemsSt.length; i++) {
-		if (itemsSt[i].classList.contains(this.value)) {
-			itemsSt[i].style.display = 'block';
-		} else {
-			itemsSt[i].style.display = 'none';
-		}
-	}
+  var itemsSt = itemsElementsStatus.getElementsByClassName('item');
+  for (var i = 0; i < itemsSt.length; i++) {
+    if (itemsSt[i].classList.contains(this.value)) {
+      itemsSt[i].style.display = 'block';
+    } else {
+      itemsSt[i].style.display = 'none';
+    }
+  }
 };
 
 //Filter by priority
 var filterPriority = document.getElementById('select-priority');
 var itemsElementsPriority = document.getElementById('tasks');
 filterPriority.onchange = function () {
-	var items = itemsElementsPriority.getElementsByClassName('item');
-	for (var i = 0; i < items.length; i++) {
-		if (items[i].classList.contains(this.value)) {
-			items[i].style.display = 'block';
-		} else {
-			items[i].style.display = 'none';
-		}
-	}
+  var items = itemsElementsPriority.getElementsByClassName('item');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].classList.contains(this.value)) {
+      items[i].style.display = 'block';
+    } else {
+      items[i].style.display = 'none';
+    }
+  }
 };
 
 //Filter for title
 function myFunction() {
-	var inputSearch = document.getElementById('search');
-	var filter = inputSearch.value.toUpperCase();
-	var ul = document.getElementById('tasks');
-	var li = ul.getElementsByTagName('li');
-	for (var i = 0; i < li.length; i++) {
-		var title = li[i].getElementsByTagName('title')[0];
-		var txtValue = title.textContent || title.innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-			li[i].style.display = 'block';
-		} else {
-			li[i].style.display = 'none';
-		}
-	}
+  var inputSearch = document.getElementById('search');
+  var filter = inputSearch.value.toUpperCase();
+  var ul = document.getElementById('tasks');
+  var li = ul.getElementsByTagName('li');
+  for (var i = 0; i < li.length; i++) {
+    var title = li[i].getElementsByTagName('title')[0];
+    var txtValue = title.textContent || title.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = 'block';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
 }
--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 let modal = document.getElementById('createModal');
 let createButton = document.getElementById('create');
 let saveButton = document.getElementById('save');
@@ -14,58 +13,58 @@ let searchTitle = document.getElementById('search');
 
 //Open modal window with form for create new task
 createButton.onclick = function () {
-	modal.style.display = "block";
+   modal.style.display = "block";
 }
 //Close modal window and clear inputs after click on button "Cancel"
 function closeModal(){
-	modal.style.display = "none";
-    document.getElementById('new-title').value = '';
-    document.getElementById('new-description').value = '';
+   modal.style.display = "none";
+   document.getElementById('new-title').value = '';
+   document.getElementById('new-description').value = '';
 }
 cancelButton.onclick = closeModal;
 
 //
 function createNewElement(title, description, priority){
-	let listItem = document.createElement('li');
-	listItem.className = 'task' + ' ' + 'item' + ' ' + priority;
-	listItem.style.display = 'block';
-	let titleTask = document.createElement('title');
-	titleTask.innerText = title;
-	let descriptionTask = document.createElement('description');
-	descriptionTask.innerText = description;
-	let priorityTask = document.createElement('priority');
-	priorityTask.innerText = priority;
-	let multiButton = document.createElement('button');
-	multiButton.innerText = '...';
-	let listContainer = document.createElement('ul');
-	listContainer.className = 'list-container';
-	let doneButton = document.createElement('li');
-	doneButton.innerText = 'Done';
-	doneButton.className = 'done';
-	let editButton = document.createElement('li');
-	editButton.innerText = 'Edit';
-	editButton.className = 'edit';
-	let deleteButton = document.createElement('li');
-	deleteButton.innerText = 'Delete';
-	deleteButton.className = 'delete';
-	listItem.appendChild(titleTask);
-	listItem.appendChild(descriptionTask);
-	listItem.appendChild(priorityTask);
-	listItem.appendChild(multiButton);
-	listItem.appendChild(listContainer);
-	listContainer.appendChild(doneButton);
-	listContainer.appendChild(editButton);
-	listContainer.appendChild(deleteButton);
-	multiButton.onclick = openList;
-	return listItem;
+   let listItem = document.createElement('li');
+   listItem.className = 'task' + ' ' + 'item' + ' ' + priority;
+   listItem.style.display = 'block';
+   let titleTask = document.createElement('title');
+   titleTask.innerText = title;
+   let descriptionTask = document.createElement('description');
+   descriptionTask.innerText = description;
+   let priorityTask = document.createElement('priority');
+   priorityTask.innerText = priority;
+   let multiButton = document.createElement('button');
+   multiButton.innerText = '...';
+   let listContainer = document.createElement('ul');
+   listContainer.className = 'list-container';
+   let doneButton = document.createElement('li');
+   doneButton.innerText = 'Done';
+   doneButton.className = 'done';
+   let editButton = document.createElement('li');
+   editButton.innerText = 'Edit';
+   editButton.className = 'edit';
+   let deleteButton = document.createElement('li');
+   deleteButton.innerText = 'Delete';
+   deleteButton.className = 'delete';
+   listItem.appendChild(titleTask);
+   listItem.appendChild(descriptionTask);
+   listItem.appendChild(priorityTask);
+   listItem.appendChild(multiButton);
+   listItem.appendChild(listContainer);
+   listContainer.appendChild(doneButton);
+   listContainer.appendChild(editButton);
+   listContainer.appendChild(deleteButton);
+   multiButton.onclick = openList;
+   return listItem;
 }
 
 //
 function addTask(){
-		let listItem = createNewElement(newTitle.value, newDescription.value, priorityTask.value);
-		tasksList.appendChild(listItem);
-		bindTaskEvent(listItem, doneTask);
-		closeModal();
+       let listItem = createNewElement(newTitle.value, newDescription.value, priorityTask.value);
+       tasksList.appendChild(listItem);
+       bindTaskEvent(listItem, doneTask);
+       closeModal();
 }
 
 //
@@ -73,103 +72,103 @@ saveButton.onclick = addTask;
 
 //Click button for open list with edit buttons
 function openList(){
-	let container = this.parentNode;
-	let list = container.querySelector('ul.list-container');
-	list.style.display = (list.style.display == 'block') ? 'none' : 'block'
+   let container = this.parentNode;
+   let list = container.querySelector('ul.list-container');
+   list.style.display = (list.style.display == 'block') ? 'none' : 'block'
 }
 
 //Button done
 function doneTask(){
-	let listItem = this.parentNode;
-	let taskBody = listItem.parentNode;
-	let doneButton = listItem.querySelector('li.done');
-	let checkBox = document.createElement('input');
-	checkBox.setAttribute('type', 'checkbox');
-	checkBox.checked = true;
-	taskBody.appendChild(checkBox);
-	taskBody.className = 'task-done' + ' ' + 'item' + ' ' + 'priority';	
-	checkBox.onclick = unDoneTask;
+   let listItem = this.parentNode;
+   let taskBody = listItem.parentNode;
+   let doneButton = listItem.querySelector('li.done');
+   let checkBox = document.createElement('input');
+   checkBox.setAttribute('type', 'checkbox');
+   checkBox.checked = true;
+   taskBody.appendChild(checkBox);
+   taskBody.className = 'task-done' + ' ' + 'item' + ' ' + 'priority';    
+   checkBox.onclick = unDoneTask;
 }
 //Mark task as not completed
 function unDoneTask(){
-	let li = this.parentNode;
-	li.className = 'task' + ' ' + 'item' + ' ' + 'priority';
-	let checked = li.querySelector('input');
-	li.removeChild(checked);
+   let li = this.parentNode;
+   li.className = 'task' + ' ' + 'item' + ' ' + 'priority';
+   let checked = li.querySelector('input');
+   li.removeChild(checked);
 }
 function editTask(){
-	let list = this.parentNode;
-	let ul = list.parentNode;
-	let task = ul.parentNode;
-	modal.style.display = 'block';
-	let changeTitle = task.querySelector('title').textContent;
-	let changeDescription = task.querySelector('description').textContent;
-	newTitle.value = changeTitle;
-	newDescription.value = changeDescription;
-	task.removeChild(ul);
+  let list = this.parentNode;
+  let ul = list.parentNode;
+  let task = ul.parentNode;
+  modal.style.display = 'block';
+  let changeTitle = task.querySelector('title').textContent;
+  let changeDescription = task.querySelector('description').textContent;
+  newTitle.value = changeTitle;
+  newDescription.value = changeDescription;
+  task.removeChild(ul);
 }
 //Button delete task
 function deleteTask(){
-	let list = this.parentNode;
-	let ul = list.parentNode;
-	let task = ul.parentNode;
-	task.removeChild(ul);
+  let list = this.parentNode;
+  let ul = list.parentNode;
+  let task = ul.parentNode;
+  task.removeChild(ul);
 }
 function bindTaskEvent(listItem, statusEvent){
-	let doneButton = listItem.querySelector('li.done');
-	let editButton = listItem.querySelector('li.edit');
-	let deleteButton = listItem.querySelector('li.delete');
+  let doneButton = listItem.querySelector('li.done');
+  let editButton = listItem.querySelector('li.edit');
+  let deleteButton = listItem.querySelector('li.delete');
 
-	doneButton.onclick = doneTask;
-	editButton.onclick = editTask;
-	deleteButton.onclick = deleteTask;
+  doneButton.onclick = doneTask;
+  editButton.onclick = editTask;
+  deleteButton.onclick = deleteTask;
 }
 
 //Filter by status
 let filterStatus = document.getElementById('select-status');
 let itemsElementsStatus = document.getElementById('tasks');
 filterStatus.onchange = function(){
-	let itemsSt = itemsElementsStatus.getElementsByClassName('item');
-	for (let i = 0; i < itemsSt.length; i++) {
-  	if (itemsSt[i].classList.contains(this.value)) {
-    	itemsSt[i].style.display = 'block';
-    } 
-    else {
-    	itemsSt[i].style.display = 'none';
-    }
+  let itemsSt = itemsElementsStatus.getElementsByClassName('item');
+  for (let i = 0; i < itemsSt.length; i++) {
+    if (itemsSt[i].classList.contains(this.value)) {
+      itemsSt[i].style.display = 'block';
+  } 
+  else {
+      itemsSt[i].style.display = 'none';
   }
+}
 }
 
 //Filter by priority
 let filterPriority = document.getElementById('select-priority');
 let itemsElementsPriority = document.getElementById('tasks');
 filterPriority.onchange = function(){
-	let items = itemsElementsPriority.getElementsByClassName('item');
-	for (let i = 0; i < items.length; i++) {
-  	if (items[i].classList.contains(this.value)) {
-    	items[i].style.display = 'block';
-    } 
-    else {
-    	items[i].style.display = 'none';
-    }
+  let items = itemsElementsPriority.getElementsByClassName('item');
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].classList.contains(this.value)) {
+      items[i].style.display = 'block';
+  } 
+  else {
+      items[i].style.display = 'none';
   }
+}
 }
 
 
 //Filter for title
 function myFunction() {
-    let inputSearch = document.getElementById('search');
-    let filter = inputSearch.value.toUpperCase();
-    let ul = document.getElementById('tasks');
-    let li = ul.getElementsByTagName('li');
-    for (let i = 0; i < li.length; i++) {
-        let title = li[i].getElementsByTagName('title')[0];
-        let txtValue = title.textContent || title.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = 'block';
-        } 
-        else {
-            li[i].style.display = 'none';
-        }
-    }
+  let inputSearch = document.getElementById('search');
+  let filter = inputSearch.value.toUpperCase();
+  let ul = document.getElementById('tasks');
+  let li = ul.getElementsByTagName('li');
+  for (let i = 0; i < li.length; i++) {
+      let title = li[i].getElementsByTagName('title')[0];
+      let txtValue = title.textContent || title.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = 'block';
+      } 
+      else {
+         li[i].style.display = 'none';
+     }
+ }
 }
